af94df03c705a1acecca068b491538d41c3c1dd9
Elvis DARKO <elvis_darko@outlook.com>
2023-08-22 08:51:52 +0000

    API codes added #2

--- src/main.py
+++ src/main.py
@@ -7,3 +7,24 @@ import uvicorn
 from fastapi.responses import JSONResponse
 
 app = FastAPI()
+
+
+# Load the numerical imputer, scaler, and model
+num_imputer_filepath = "/com.docker.devenvironments.code/project_directory/ML components/numerical_imputer.joblib"
+scaler_filepath = "/com.docker.devenvironments.code/project_directory/ML components/scaler.joblib"
+model_filepath = "/com.docker.devenvironments.code/project_directory/ML components/rf_model.joblib"
+
+num_imputer = joblib.load(num_imputer_filepath)
+scaler = joblib.load(scaler_filepath)
+model = joblib.load(model_filepath)
+
+class PatientData(BaseModel):
+    PRG: float
+    PL: float
+    PR: float
+    SK: float
+    TS: float
+    M11: float
+    BD2: float
+    Age: float
+    Insurance: int
